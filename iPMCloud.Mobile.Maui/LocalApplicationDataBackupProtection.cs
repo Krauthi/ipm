d138b75737425ec63b15372b84d0dea34997c32f
Markus Krauthausen <office@ipm-cloud.de>
2026-06-18 11:31:04 +0200

    Add LocalApplicationData backup protection helper

---
+++ iPMCloud.Mobile.Maui/LocalApplicationDataBackupProtection.cs
@@ -0,0 +1,35 @@
+using Foundation;
+using System;
+using System.IO;
+
+namespace iPMCloud.Mobile;
+
+public static partial class LocalApplicationDataBackupProtection
+{
+    public static string LocalApplicationDataPath =>
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+    public static void EnsureExcludedFromBackup()
+    {
+        var path = LocalApplicationDataPath;
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        ExcludePathFromBackup(path);
+    }
+
+    public static void EnsureExcludedFromBackup(string fileOrDirectoryPath)
+    {
+        if (string.IsNullOrWhiteSpace(fileOrDirectoryPath))
+        {
+            return;
+        }
+
+        ExcludePathFromBackup(fileOrDirectoryPath);
+    }
+
+    static partial void ExcludePathFromBackup(string path);
+}
